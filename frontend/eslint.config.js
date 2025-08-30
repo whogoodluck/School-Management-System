f20207efe5014d52759eb874dd8f3e9b81a61796
sunny <goodluck14115@gmail.com>
2025-08-30 20:54:42 +0530

    chore: setup prettier and eslint for better code formating and linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,27 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-])
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  }
+)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-red-500 font-bold">
-        School Management System
-      </h1>
+      <h1 className='text-3xl font-bold text-red-500'>School Management System</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
