ea32948e251dfa484245de845415577cc7df9803
sunny <goodluck14115@gmail.com>
2025-08-30 20:41:37 +0530

    Setup Vite + React with TailwindCSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500 font-bold">
+        School Management System
+      </h1>
+    </div>
+  );
+}
+
+export default App;
