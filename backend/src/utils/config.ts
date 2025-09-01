b49732895823d57ff9103b94fb29a9645a3d8b32
sunny <goodluck14115@gmail.com>
2025-09-02 00:23:49 +0530

    Update config

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -2,13 +2,12 @@ import { config } from 'dotenv'
 
 config()
 
-const PORT = process.env.PORT || 3001
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3002'
+const PORT = process.env.PORT || 3002
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
+const FRONTEND_URL = process.env.FRONTEND_URL
 
 export default {
   PORT,
-  BASE_URL,
+
   FRONTEND_URL
 }
